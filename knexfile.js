ba24552220d2ec06e23da42f25fb11e755203689
Bradley Eggins <brad.eggins@gmail.com>
2020-09-23 19:44:33 +1200

    Knex initilisation

---
+++ knexfile.js
@@ -0,0 +1,65 @@
+const path = require('path')
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: path.join(__dirname, './server/database/dev.sqlite3')
+    },
+    migrations: {
+      directory: path.join(__dirname, '/server/database/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, '/server/database/seeds')
+    }
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory'
+    },
+    migrations: {
+      directory: path.join(__dirname, 'server/database/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'server/database/seeds')
+    }
+  },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: path.join(__dirname, 'server/database/migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'server/database/seeds')
+    }
+  }
+
+}

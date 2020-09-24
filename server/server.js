2ea0b78034f3ecf1fba8a421cfc16e0f053d5d8c
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-24 16:21:56 +1200

    [routes] POST/ Save New Goal

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const express = require('express')
 
 const server = express()
 const auth = require('./routes/auth')
+const goals = require('./routes/goals')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+server.use('/api/v1/goals', goals)
 server.use('/api/v1', auth)
 
 module.exports = server

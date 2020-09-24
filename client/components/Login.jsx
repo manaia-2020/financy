888719261ce5430614996dedc5415d14465222c8
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-25 11:13:21 +1200

    fix linting issues

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -23,7 +23,7 @@ const Login = (props) => {
     signIn({ username: email, password }, { baseUrl })
       .then((token) => {
         if (isAuthenticated()) {
-            console.log('Logged in');
+          console.log('Logged in')
           props.history.push('/register')
         }
         return null

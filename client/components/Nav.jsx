5aafdc9d67db6ae919f782fe6a93e276662306be
Tia TeHiko <tiatehiko@gmail.com>
2020-09-24 16:51:49 +1200

    linting

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const Nav = () => {
   return (
     <header id="header" className="fixed-top ">
       <div className="container-fluid"></div>
       <h1 className="logo mr-auto">Financy</h1>
       <ul>
-
         <Link to='/' className="active"><li>Home</li></Link>
         <Link to='/about'><li>About</li></Link>
         <Link to='/team'><li>Team</li></Link>

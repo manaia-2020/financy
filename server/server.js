82a0fe378d1aeba443f9606d1f4f5a9d187c86df
Tia TeHiko <tiatehiko@gmail.com>
2020-09-25 08:07:32 +1200

    import and use account routes

--- server/server.js
+++ server/server.js
@@ -2,10 +2,14 @@ const path = require('path')
 const express = require('express')
 
 const server = express()
+
 const auth = require('./routes/auth')
+const accounts = require('./routes/accounts')
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
+
 server.use('/api/v1', auth)
+server.use('/api/v1/accounts', accounts)
 
 module.exports = server

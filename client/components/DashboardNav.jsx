f96003b32476dfe10a19bcedfe348d97b463d4d3
Tia TeHiko <tiatehiko@gmail.com>
2020-09-24 16:01:24 +1200

    create nav bars

---
+++ client/components/DashboardNav.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+const DashboardNav = () => {
+  return (
+    <div>
+      <Link to='/'><h4>Profile</h4></Link>
+      <Link to='/rewards'><h4>Rewards</h4></Link>
+      <Link to='/goals'><h4>Goals</h4></Link>
+      <Link to='/income'><h4>Income</h4></Link>
+      <Link to='/expenses'><h4>Expenses</h4></Link>
+    </div>
+  )
+}
+
+export default DashboardNav
--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 
-const NavBar = () => {
+const Nav = () => {
   return (
     <div>
       <Link to='/'><h4>HOME</h4></Link>
@@ -14,4 +14,4 @@ const NavBar = () => {
   )
 }
 
-export default NavBar
+export default Nav

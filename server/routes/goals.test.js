b6b4f70c3a92e7c94d5fc8ffcb038556ce885150
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-25 22:54:53 +1200

    [test][routes] GET /api/v1/goals/:id -> status 200

--- server/routes/goals.test.js
+++ server/routes/goals.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable promise/always-return */
 import request from 'supertest'
-import { saveNewGoal } from '../database/goals.database'
+import { saveNewGoal, getAllGoalsByUserId } from '../database/goals.database'
 import server from '../server'
 
 jest.mock('../database/goals.database', () => ({
-  saveNewGoal: jest.fn()
+  saveNewGoal: jest.fn(),
+  getAllGoalsByUserId: jest.fn()
 }))
 
 describe('POST/ api/v1/goals', () => {
@@ -26,6 +27,20 @@ describe('POST/ api/v1/goals', () => {
       .then(() => {
         expect(saveNewGoal).toHaveBeenCalled()
         expect(saveNewGoal).toHaveBeenCalledWith(data)
+      })
+  })
+})
+
+describe('GET/ api/v1/goals/:id', () => {
+  const id = 2
+  test('check if status 200', () => {
+    getAllGoalsByUserId.mockImplementation(() => Promise.resolve())
+    return request(server)
+      .get(`/api/v1/goals/${id}`)
+      .then((res) => {
+        expect.assertions(1)
+        expect(res.status).toBe(201)
+        return null
       })
   })
 })

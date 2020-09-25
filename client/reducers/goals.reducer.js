db4686bff329a2e27858f9e65a0ae25d99213a78
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-26 11:20:01 +1200

    [reducers][actions] fetching user goals

--- client/reducers/goals.reducer.js
+++ client/reducers/goals.reducer.js
@@ -1,4 +1,4 @@
-import { ADD_GOAL } from '../actions/goals.action'
+import { ADD_GOAL, FETCH_GOALS_FAILURE, FETCH_GOALS_SUCCESS } from '../actions/goals.action'
 
 const initialState = []
 
@@ -11,6 +11,12 @@ export default function goalsReducer (state = initialState, action) {
         amount: action.payload.goal.amount,
         user_id: action.payload.id
       }]
+
+    case FETCH_GOALS_SUCCESS:
+      return action.payload.goals
+
+    case FETCH_GOALS_FAILURE:
+      return state
 
     default:
       return state

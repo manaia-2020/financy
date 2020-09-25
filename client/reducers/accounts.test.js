7661d4c58020f9d09fa9021adbbed1ab7cb082d5
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-25 13:36:31 +1200

    [test][reducers] GET_ACCOUNTS

--- client/reducers/accounts.test.js
+++ client/reducers/accounts.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable jest/valid-title */
 import accountsReducer from './accounts.reducer'
-import { addAccount, ADD_ACCOUNT } from '../actions/accounts.action'
+import { addAccount, ADD_ACCOUNT, getAccounts, GET_ACCOUNTS } from '../actions/accounts.action'
 
 describe('account reducer tests', () => {
   test(ADD_ACCOUNT, () => {
@@ -30,6 +30,28 @@ describe('account reducer tests', () => {
     const action = addAccount(account, 2)
 
     const actualState = accountsReducer(initialState, action)
+
+    expect(actualState).toEqual(expectedState)
+  })
+
+  test(GET_ACCOUNTS, () => {
+    expect.assertions(1)
+    const accounts = [{
+      name: 'Jesus',
+      balance: '🍷',
+      user_id: 1
+    },
+    {
+      name: 'Mary',
+      balance: '🐑',
+      user_id: 2
+    }]
+
+    const expectedState = [...accounts]
+
+    const action = getAccounts(accounts)
+
+    const actualState = accountsReducer([], action)
 
     expect(actualState).toEqual(expectedState)
   })

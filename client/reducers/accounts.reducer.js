3e5dd1479f20a667003ea2ed89d91df25db7ec51
Tia TeHiko <tiatehiko@gmail.com>
2020-09-26 17:13:03 +1200

    delete account

--- client/reducers/accounts.reducer.js
+++ client/reducers/accounts.reducer.js
@@ -1,8 +1,8 @@
-import { GET_ACCOUNTS, ADD_ACCOUNT } from '../actions/accounts.action'
+import { GET_ACCOUNTS, ADD_ACCOUNT, DELETE_ACCOUNT } from '../actions/accounts.action'
 
 const initialState = []
 
-export default function accountsReducer (state = initialState, action) {
+export default function accountsReducer(state = initialState, action) {
   switch (action.type) {
     case GET_ACCOUNTS:
       return action.accounts
@@ -13,6 +13,8 @@ export default function accountsReducer (state = initialState, action) {
         balance: action.account.balance,
         user_id: action.id
       }]
+    case DELETE_ACCOUNT:
+      return state.filter((account) => account.id !== action.id)
     default:
       return state
   }

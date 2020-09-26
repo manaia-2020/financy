73609142d26bc01dedabef11110d3b5efadb5171
Bradley Eggins <brad.eggins@gmail.com>
2020-09-26 17:51:37 +1200

    Refine seeds, migrations and initial transactions fnc and test

---
+++ server/database/transactionsDb.test.js
@@ -0,0 +1,22 @@
+const knex = require('knex')
+const config = require('../../knexfile').test
+const { getAllTransForUser } = require('./transactionsDb')
+
+const testDb = knex(config)
+
+beforeAll(() => testDb.migrate.latest())
+beforeEach(() => testDb.seed.run())
+
+describe('getTransForUser', () => {
+  test('Returns all transactions for userId and accountId', () => {
+    const userId = 2
+    const accountId = 6
+    expect.assertions(2)
+    return getAllTransForUser(userId, accountId, testDb)
+      .then(trans => {
+        expect(trans).toHaveLength(4)
+        expect(trans[0].name).toBe('BK')
+        return null
+      })
+  })
+})

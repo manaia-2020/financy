fb6a89aa71931d0aab5ac1fddefae853f65bea59
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-29 10:02:15 +1300

    add production connections

--- client/config.js
+++ client/config.js
@@ -1,12 +1,12 @@
 const environments = {
   development: 'http://localhost:3000/api/v1',
   test: 'http://localhost:3333/api/v1',
-  production: '',
+  production: 'http://realfinancy.herokuapp.com',
   integration: '',
   deployment: '',
   build: ''
 }
 
-const env = process.env.NODE_ENV || 'development'
+const env = 'production'
 
 export const baseApiUrl = environments[env]

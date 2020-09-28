61a9b70cf1c2ab172dc55a3740e8c8458cbca894
Your Namemax-remacle <remacle.maxime@gmail.com>
2020-09-28 16:53:51 +1300

    started styling on addtrans component

--- client/components/AddTransaction.jsx
+++ client/components/AddTransaction.jsx
@@ -2,11 +2,51 @@ import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { addNewTransaction } from '../api/api'
 
+import Avatar from '@material-ui/core/Avatar'
+import Button from '@material-ui/core/Button'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import TextField from '@material-ui/core/TextField'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Checkbox from '@material-ui/core/Checkbox'
+import Link from '@material-ui/core/Link'
+import Grid from '@material-ui/core/Grid'
+import Box from '@material-ui/core/Box'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles } from '@material-ui/core/styles'
+import Container from '@material-ui/core/Container'
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from "@material-ui/core/InputLabel";
+import Select from "@material-ui/core/Select";
+import MenuItem from "@material-ui/core/MenuItem";
+
 const AddTransaction = (props) => {
   useEffect(() => {
 
   }, [])
 
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: '#17E9E0'
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+      borderColor: '#A64AC9'
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+      backgroundColor: '#17E9E0'
+    }
+  
+  }))
+  
   const [newTrans, setNewTrans] = useState({
     amount: 0,
     name: '',
@@ -54,6 +94,7 @@ const AddTransaction = (props) => {
     )
   }
 
+  const classes = useStyles()
   return (
     <div>
       <h1>Add New Transaction</h1>
@@ -73,6 +114,60 @@ const AddTransaction = (props) => {
         <button type="submit" disabled={!checkFloat()}>Add New Trans</button>
         {(showRecurring) ? viewRecurringForm() : null }
       </form>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Typography component="h1" variant="h5">
+            Amount
+          </Typography>
+          <form className={classes.form} noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="amount"
+              label="amount"
+              name="amount"
+              autoFocus
+              onChange={handleChange}>
+            </TextField>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="expenseName"
+              label="expenseName"
+              name="expenseName"
+              autoFocus
+              onChange={handleChange}>
+            </TextField>
+            <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel id="accountSelect"></InputLabel>
+            <Select
+              labelId="accountSelect"
+              id="accountSelect"
+              value=""
+              onChange={handleChange}
+              label="accountSelect"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              {props.accounts.map(account => <MenuItem key={account.id} value={account.id}>{account.name}</MenuItem>)}
+            </Select>
+          </FormControl>
+            <Grid container>
+            </Grid>
+          </form>
+        </div>
+        <Box mt={8}>
+        </Box>
+      </Container>
+     
+
+
     </div>
   )
 }

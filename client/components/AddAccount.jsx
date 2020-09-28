2960c2ddc7ef5a7d57385c889122ac112447d30e
Tia TeHiko <tiatehiko@gmail.com>
2020-09-28 13:02:32 +1300

    refactoring code

--- client/components/AddAccount.jsx
+++ client/components/AddAccount.jsx
@@ -4,19 +4,19 @@ import { addAccount } from '../actions/accounts.action'
 import { postAccount } from '../api/api'
 import { formatAmount, localStringToNumber } from '../utils/currency'
 
-const AddAccount = (props) => {
+function AddAccount(props) {
   const [account, setAccount] = useState({
     name: '',
     balance: ''
   })
 
-  const handleChange = (event) => {
+  function handleChange(event) {
     event.preventDefault()
     const { name, value } = event.target
     setAccount({ ...account, [name]: value })
   }
 
-  const handleSubmit = (event) => {
+  function handleSubmit(event) {
     const id = props.userInfo.id
     event.preventDefault()
     props.dispatch(addAccount(account, id))
@@ -25,7 +25,7 @@ const AddAccount = (props) => {
       .catch(err => console.log(err))
   }
 
-  function handleBlur ({ target }) {
+  function handleBlur({ target }) {
     const { name, value } = target
     setAccount({
       ...account,
@@ -33,7 +33,7 @@ const AddAccount = (props) => {
     })
   }
 
-  function handleFocus ({ target }) {
+  function handleFocus({ target }) {
     const { name, value } = target
     setAccount({
       ...account,

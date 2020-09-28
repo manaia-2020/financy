54c70846ea278b440ca99c78c0658ef09887b425
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-29 10:45:12 +1300

    update config

--- client/config.js
+++ client/config.js
@@ -1,7 +1,7 @@
 const environments = {
-  development: 'http://localhost:3000/api/v1',
-  test: 'http://localhost:3333/api/v1',
-  production: 'http://realfinancy.herokuapp.com/api/v1',
+  development: '/api/v1',
+  test: '/api/v1',
+  production: '/api/v1',
   integration: '',
   deployment: '',
   build: ''

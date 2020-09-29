a3f596b65c029df99627fe9446352f8b176821c0
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-30 10:40:47 +1300

    goal deletion functionality and add goal fix

--- server/routes/goals.js
+++ server/routes/goals.js
@@ -1,15 +1,15 @@
 const express = require('express')
-const { saveNewGoal, getAllGoalsByUserId } = require('../database/goalsDb')
+const { getTokenDecoder } = require('authenticare/server')
+const { saveNewGoal, getAllGoalsByUserId, deleteGoalById, getGoalById } = require('../database/goalsDb')
 const router = express.Router()
 
 router.post('/', (req, res) => {
   const { name, date, amount, id } = req.body
 
   saveNewGoal({ name, date, amount, id })
-    .then((id) => {
-      return res.status(201).json(id[0])
-    })
-    .catch(err => console.log(err))
+    .then((id) => getGoalById(id))
+    .then((goal) => res.status(201).json(goal))
+    .catch(() => res.status(500).send('DATABASE ERROR'))
 })
 
 router.get('/:id', (req, res) => {
@@ -18,6 +18,19 @@ router.get('/:id', (req, res) => {
   getAllGoalsByUserId(id)
     .then((goals) => res.status(201).json(goals))
     .catch(() => res.status(500).send('DATABASE ERROR'))
+})
+
+router.delete('/', getTokenDecoder(), async (req, res) => {
+  try {
+    if (req.user) {
+      const id = req.body.id
+      const goal = await getGoalById(id)
+      if (goal.user_id === req.user.id) await deleteGoalById(id)
+      res.sendStatus(201)
+    }
+  } catch (err) {
+    res.sendStatus(500)
+  }
 })
 
 module.exports = router

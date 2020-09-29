66b4fa0bb0a617f690c05851883d62a26ac7266d
Tia TeHiko <tiatehiko@gmail.com>
2020-09-29 14:04:49 +1300

    import images for landing page

---
+++ client/components/LandingPage/modules/views/ProductHero.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Button from '../components/Button'
+import Typography from '../components/Typography'
+import ProductHeroLayout from './ProductHeroLayout'
+
+const backgroundImage =
+  'https://images.unsplash.com/photo-1534854638093-bada1813ca19?auto=format&fit=crop&w=1400&q=80'
+
+const styles = (theme) => ({
+  background: {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundColor: '#7fc7d9', // Average color of the background image.
+    backgroundPosition: 'center'
+  },
+  button: {
+    minWidth: 200
+  },
+  h5: {
+    marginBottom: theme.spacing(4),
+    marginTop: theme.spacing(4),
+    [theme.breakpoints.up('sm')]: {
+      marginTop: theme.spacing(10)
+    }
+  },
+  more: {
+    marginTop: theme.spacing(2)
+  }
+})
+
+function ProductHero(props) {
+  const { classes } = props
+
+  return (
+    <ProductHeroLayout backgroundClassName={classes.background}>
+      {/* Increase the network loading priority of the background image. */}
+      <img
+        style={{ display: 'none' }}
+        src={backgroundImage}
+        alt="increase priority"
+      />
+      <Typography color="inherit" align="center" variant="h2" marked="center">
+        Budget your way to prosperity
+      </Typography>
+      <Typography
+        color="inherit"
+        align="center"
+        variant="h5"
+        className={classes.h5}
+      >
+        A fun and Interactive way to save money and manage your finances
+      </Typography>
+      <Button
+        color="secondary"
+        variant="contained"
+        size="large"
+        className={classes.button}
+        component="a"
+        href="/#/register"
+      >
+        Register
+      </Button>
+    </ProductHeroLayout>
+  )
+}
+
+ProductHero.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(ProductHero)

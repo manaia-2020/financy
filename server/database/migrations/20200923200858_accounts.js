410ec2f59bda27b666518116b14ec64370891916
Josiah Waikari <teariki.waikari@gmail.com>
2020-09-30 13:33:19 +1300

    [database] update accounts table to have balance column

--- server/database/migrations/20200923200858_accounts.js
+++ server/database/migrations/20200923200858_accounts.js
@@ -2,6 +2,7 @@ exports.up = function (knex) {
   return knex.schema.createTable('accounts', (table) => {
     table.increments('id').primary()
     table.string('name')
+    table.float('balance')
     table.integer('user_id')
     table.foreign('user_id').references('id').inTable('users')
   })
